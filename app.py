6c5acc0f73cfc7a8678c14ae7d349b30bb50405a
MukunthanML <mukunthan.r2@gmail.com>
2020-04-27 19:49:48 +0530

    app.run() is removed from if block

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ def removeProduct(name):
 
     return jsonify({'Error': 'Product {} not found !'.format(name)})
 
-#
-# if __name__=='__main__':
-#     app.run()
 
-app.run()
+if __name__=='__main__':
+    app.run()
+
